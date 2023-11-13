5c97ab73729616fe5d94f7b92dd21f2f07368ec4
Reedtztoja <patryk.patryg@gmail.com>
2023-11-13 12:45:35 +0100

    POST

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 const db = require('./db');
 
 app.get('/', async (req, res) => {
@@ -21,5 +23,24 @@ app.get('/listAll', async (req, res) => {
     db.close(client);
     res.end();
 })
+app.post('/list', async (req, res) => {
+    // console.log(req.body);
+    // res.sendStatus(200);
+    res.contentType('text/html');
+    res.write("<h1>Lista wyszukanych rekordow wedlug kryteriow:</h1>");
+    const client = await db.connect();
+    res.write("<table>");
+    let list = await db.get(client, req.body);
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url + "</td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("</tr>");
+    });
+    res.write("</table>");
+    db.close(client);
+    res.end();
+})
+
 
 app.listen(8000);
