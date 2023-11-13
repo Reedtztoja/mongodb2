9aca53f037e6957fc62b5c1c8a92bd440545cd07
Reedtztoja <patryk.patryg@gmail.com>
2023-11-13 12:45:44 +0100

    POST2

--- db.js
+++ db.js
@@ -21,8 +21,15 @@ async function getAllListings(client) {
     let list = collection.find().toArray();
     return list;
 }
+
+async function get(client, criteria) {
+    const collection = await client.db('sample_airbnb').collection('listingsAndReviews');
+    let list = collection.find(criteria).toArray();
+    return list;
+}
+
 function close(client) {
     client.close();
     console.log("Successfully disconnected from MongoDB");
 }
-module.exports = {connect, getAllListings, close}
+module.exports = {connect, getAllListings, close, get}
